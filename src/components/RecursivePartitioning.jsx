110b1e4e14d06680f09afab2a3a38c0fca282052
Samirun-Shuvo <samirunshuvo@gmail.com>
2024-09-10 17:58:31 +0600

    initial commit for project set up

---
+++ src/components/RecursivePartitioning.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecursivePartitioning = () => {
+    return (
+        <div>
+            RecursivePartitioning
+        </div>
+    );
+};
+
+export default RecursivePartitioning;
